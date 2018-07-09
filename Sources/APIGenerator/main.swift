fc4c187992947ff6857f785146c163d603257b20
Eneko Alonso <eneko.alonso@gmail.com>
2018-07-09 09:16:12 -0700

    Nested objects for race->circuit

--- Sources/APIGenerator/main.swift
+++ Sources/APIGenerator/main.swift
@@ -3,6 +3,11 @@ import MySQL
 import GPSModels
 import DotEnv
 
+enum GeneratorError: Error {
+    case notFound
+    case serializationError
+}
+
 let env = DotEnv(withFile: ".env")
 
 struct GPSDB: ConnectionOption {
@@ -13,14 +18,49 @@ struct GPSDB: ConnectionOption {
     let database: String = env.get("DB_NAME") ?? ""
 }
 
-let pool = ConnectionPool(options: GPSDB())
-let rows: [F1Circuit] = try pool.execute { conn in
-	try conn.query("SELECT * FROM gps_circuits;")
+func encode<T: Encodable>(rows: T) throws -> String {
+    let encoder = JSONEncoder()
+    encoder.outputFormatting = .prettyPrinted
+    let data = try encoder.encode(rows)
+    guard let json = String.init(data: data, encoding: .utf8) else {
+        throw GeneratorError.serializationError
+    }
+    return json
 }
 
-let encoder = JSONEncoder()
-encoder.outputFormatting = .prettyPrinted
-let data = try encoder.encode(rows)
-if let json = String.init(data: data, encoding: .utf8) {
-    print(json)
+func loadCircuits(pool: ConnectionPool) throws -> [String: F1Circuit] {
+    let rows: [F1Circuit] = try pool.execute { conn in
+        try conn.query("SELECT * FROM gps_circuits;")
+    }
+    return Dictionary(uniqueKeysWithValues: rows.map { ($0.circuitRef, $0) })
 }
+
+func loadCircuits(refs: [String], pool: ConnectionPool) throws -> [String: F1Circuit] {
+    let rows: [F1Circuit] = try pool.execute { conn in
+        try conn.query("SELECT * FROM gps_circuits WHERE circuitRef in (?);", [QueryParameterArray(refs)])
+    }
+    return Dictionary(uniqueKeysWithValues: rows.map { ($0.circuitRef, $0) })
+}
+
+func loadRaces(pool: ConnectionPool) throws -> [String: F1Race] {
+    let rows: [F1Race] = try pool.execute { conn in
+        try conn.query("SELECT raceRef, year, round, circuitRef FROM gps_races;")
+    }
+    return Dictionary(uniqueKeysWithValues: rows.map { ($0.raceRef, $0) })
+}
+
+func loadRaces(year: Int, pool: ConnectionPool) throws -> [String: F1Race] {
+    let rows: [F1Race] = try pool.execute { conn in
+        try conn.query("SELECT raceRef, year, round, circuitRef FROM gps_races WHERE year = ?;", [year])
+    }
+    return Dictionary(uniqueKeysWithValues: rows.map { ($0.raceRef, $0) })
+}
+
+let pool = ConnectionPool(options: GPSDB())
+
+var races = try loadRaces(year: 2018, pool: pool)
+let circuits = try loadCircuits(refs: races.map { $0.value.circuitRef }, pool: pool)
+for race in races.values {
+    races[race.raceRef]?.circuit = circuits[race.circuitRef]
+}
+print(try encode(rows: races))
--- Sources/GPSModels/F1Circuit.swift
+++ Sources/GPSModels/F1Circuit.swift
@@ -1,5 +1,14 @@
 
-open class F1Circuit: Codable {
-    public var circuitRef: String?
-    public var name: String?
+public struct F1Circuit: Codable {
+    public let circuitRef: String
+    public let name: String
+}
+
+public struct F1Race: Codable {
+    public let raceRef: String
+    public let year: Int
+    public let round: Int
+    public let circuitRef: String
+
+    public var circuit: F1Circuit?
 }
